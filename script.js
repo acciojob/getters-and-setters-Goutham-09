3ee2ec126bb6f97bf7cc1b4579e514f59097fc05
Goutham-09 <140509562+Goutham-09@users.noreply.github.com>
2026-09-20 21:40:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ class Person {
     get name() {
         return this._name;
     }
+	set name(name) {
+        this._name = name;
+    }
+	get age() {
+        return this._age;
+    }
 
     set age(age) {
         this._age = age;
     }
 }
-}
+
 
 class Student extends Person {
 	study() {
